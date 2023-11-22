13d9a0939acd89524f8a2a153a620ce9a28ac3df
Ubuntu <ubuntu@ip-172-31-0-65.ap-northeast-2.compute.internal>
2023-11-22 06:19:58 +0000

    숙제 11.9 mmap, rio_readn 대체

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -5,7 +5,7 @@
  * Updated 11/2019 droh
  *   - Fixed sprintf() aliasing issue in serve_static(), and clienterror().
  */
-#include "csapp.h"  // csapp 라이브러리 포함. 네트워크 프로그래밍 관련 함수 및 정의 포함
+#include "csapp.h" // csapp 라이브러리 포함. 네트워크 프로그래밍 관련 함수 및 정의 포함
 
 // 함수 선언
 void doit(int fd);
@@ -16,27 +16,30 @@ void get_filetype(char *filename, char *filetype);
 void serve_dynamic(int fd, char *filename, char *cgiargs);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 
-int main(int argc, char **argv) {
-  int listenfd, connfd; // 리스닝 및 커넥션 파일 디스크립터
-  char hostname[MAXLINE], port[MAXLINE]; // 클라이언트 호스트명 및 포트 저장 변수
-  socklen_t clientlen; // 클라이언트 주소의 길이
-  struct sockaddr_storage clientaddr; // 클라이언트 주소 정보 저장 구조체
+int main(int argc, char **argv)
+{
+    int listenfd, connfd;                  // 리스닝 및 커넥션 파일 디스크립터
+    char hostname[MAXLINE], port[MAXLINE]; // 클라이언트 호스트명 및 포트 저장 변수
+    socklen_t clientlen;                   // 클라이언트 주소의 길이
+    struct sockaddr_storage clientaddr;    // 클라이언트 주소 정보 저장 구조체
 
-  /* 커맨드 라인 인자 검사 */
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(1); // 포트 번호가 제공되지 않으면 오류 메시지 출력 후 종료
-  }
+    /* 커맨드 라인 인자 검사 */
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1); // 포트 번호가 제공되지 않으면 오류 메시지 출력 후 종료
+    }
 
-  listenfd = Open_listenfd(argv[1]); // 지정된 포트에서 리스닝 소켓 열기
-  while (1) {
-    clientlen = sizeof(clientaddr);
-    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);  // 클라이언트 연결 수락
-    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0); // 클라이언트 정보 가져오기
-    printf("Accepted connection from (%s, %s)\n", hostname, port); // 연결된 클라이언트 정보 출력
-    doit(connfd);   // 클라이언트 요청 처리
-    Close(connfd);  // 연결 종료
-  }
+    listenfd = Open_listenfd(argv[1]); // 지정된 포트에서 리스닝 소켓 열기
+    while (1)
+    {
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);                       // 클라이언트 연결 수락
+        Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0); // 클라이언트 정보 가져오기
+        printf("Accepted connection from (%s, %s)\n", hostname, port);                  // 연결된 클라이언트 정보 출력
+        doit(connfd);                                                                   // 클라이언트 요청 처리
+        Close(connfd);                                                                  // 연결 종료
+    }
 }
 /* $end tinymain */
 
@@ -55,33 +58,40 @@ void doit(int fd)
     printf("Request headers:\n");
     printf("%s", buf);
     sscanf(buf, "%s %s %s", method, uri, version);
-    if (strcasecmp(method, "GET")) {
+    if (strcasecmp(method, "GET"))
+    {
         clienterror(fd, method, "501", "Not implemented",
-        "Tiny does not implement this method");
+                    "Tiny does not implement this method");
         return;
     }
+    
     read_requesthdrs(&rio);
 
     /* Parse URI from GET request */
     is_static = parse_uri(uri, filename, cgiargs);
-    if (stat(filename, &sbuf) < 0) {
+    if (stat(filename, &sbuf) < 0)
+    {
         clienterror(fd, filename, "404", "Not found",
-        "Tiny couldn’t find this file");
+                    "Tiny couldn’t find this file");
         return;
     }
 
-    if (is_static) { /* Serve static content */
-        if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
+    if (is_static)
+    { /* Serve static content */
+        if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode))
+        {
             clienterror(fd, filename, "403", "Forbidden",
-            "Tiny couldn’t read the file");
+                        "Tiny couldn’t read the file");
             return;
         }
         serve_static(fd, filename, sbuf.st_size);
     }
-    else { /* Serve dynamic content */
-        if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) {
+    else
+    { /* Serve dynamic content */
+        if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode))
+        {
             clienterror(fd, filename, "403", "Forbidden",
-            "Tiny couldn’t run the CGI program");
+                        "Tiny couldn’t run the CGI program");
             return;
         }
         serve_dynamic(fd, filename, cgiargs);
@@ -113,14 +123,15 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longms
 // HTTP 요청 헤더를 읽는 함수
 void read_requesthdrs(rio_t *rp)
 {
-  char buf[MAXLINE];
+    char buf[MAXLINE];
 
-  Rio_readlineb(rp, buf, MAXLINE);
-  while(strcmp(buf, "\r\n")) {
     Rio_readlineb(rp, buf, MAXLINE);
-    printf("%s", buf);
-  }
-  return;
+    while (strcmp(buf, "\r\n"))
+    {
+        Rio_readlineb(rp, buf, MAXLINE);
+        printf("%s", buf);
+    }
+    return;
 }
 
 // URI를 파싱하여 정적/동적 컨텐츠 결정 함수
@@ -128,18 +139,21 @@ int parse_uri(char *uri, char *filename, char *cgiargs)
 {
     char *ptr;
 
-    if (!strstr(uri, "cgi-bin")) { /* Static content */
+    if (!strstr(uri, "cgi-bin"))
+    { /* Static content */
         strcpy(cgiargs, "");
         strcpy(filename, ".");
         strcat(filename, uri);
-        if (uri[strlen(uri)-1] == '/')
+        if (uri[strlen(uri) - 1] == '/')
             strcat(filename, "home.html");
         return 1;
     }
-    else { /* Dynamic content */
+    else
+    { /* Dynamic content */
         ptr = index(uri, '?');
-        if (ptr) {
-            strcpy(cgiargs, ptr+1);
+        if (ptr)
+        {
+            strcpy(cgiargs, ptr + 1);
             *ptr = '\0';
         }
         else
@@ -167,18 +181,49 @@ void serve_static(int fd, char *filename, int filesize)
     printf("Response headers:\n");
     printf("%s", buf);
 
-    /* Send response body to client */
+    /*Send response body to client*/
+    /*숙제 11.9 : 정적 컨텐츠를 처리할 때 요청한 파일을 mmap와 rio_rio_readn 대신,
+     malloc, rio_readn, rio_written을 사용해서 연결 식별자에게 복사*/
+
+    /*파일 내용을 저장할 메모리 할당*/
+    srcp = (char*)Malloc(filesize);
+
+    /* 파일 열기 */
     srcfd = Open(filename, O_RDONLY, 0);
-    srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
-    Close(srcfd);
+
+    /* 파일 내용을 버퍼에 읽기 */
+    Rio_readn(srcfd, srcp, filesize);
+
+    /* 버퍼의 내용을 클라이언트에게 쓰기*/
     Rio_writen(fd, srcp, filesize);
-    Munmap(srcp, filesize);
+
+    /* 할당된 메모리 해제*/
+    Free(srcp);
+
+    /* 파일 디스크립터 닫기*/
+    Close(srcfd);
+
+    /*파일 디스크립터를 열고, 파일 내용을 가상 메모리 영역에 매핑,
+     그 내용을 네트워크로 전송 후 메모리 매핑을 해제 한 후 디스크립터를 닫음.*/
+    //    srcfd = Open(filename, O_RDONLY, 0);
+    //    srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+    //    Close(srcfd);
+    //    Rio_writen(fd, srcp, filesize);
+    //    Munmap(srcp, filesize); 
+
+    /* - mmap()은 메모리의 특정 공간에 파일을 매핑한다. 
+- 프로세스는 해당 파일을 읽기 위해서 저장매체에 접근하는 것이 아닌, 메모리의 데이터에 접근하는 방식이다.
+- 그러므로 시스템 콜, 스케줄링, 인터럽트 등 OS의 개입과 저장매체로의 접근이 필요없다.
+- 즉, 성능이 개선된다. 
+출처: https://devraphy.tistory.com/428 [개발자를 향하여:티스토리]*/
+
+
 }
 
 /*
  * get_filetype - 파일 이름에서 파일 타입을 결정하는 함수
  */
-void get_filetype(char *filename, char *filetype) 
+void get_filetype(char *filename, char *filetype)
 {
     if (strstr(filename, ".html"))
         strcpy(filetype, "text/html");
@@ -188,17 +233,16 @@ void get_filetype(char *filename, char *filetype)
         strcpy(filetype, "image/png");
     else if (strstr(filename, ".jpg"))
         strcpy(filetype, "image/jpeg");
-    else if (strstr(filename, ".mp4"))  // MP4 파일 타입 추가
+    else if (strstr(filename, ".mp4")) // 숙제 11.6 C MP4 파일 타입 추가
         strcpy(filetype, "video/mp4");
     else
         strcpy(filetype, "text/plain");
 }
 
-
 // 동적 컨텐츠를 클라이언트에게 제공하는 함수
 void serve_dynamic(int fd, char *filename, char *cgiargs)
 {
-    char buf[MAXLINE], *emptylist[] = { NULL };
+    char buf[MAXLINE], *emptylist[] = {NULL};
 
     /* Return first part of HTTP response */
     sprintf(buf, "HTTP/1.0 200 OK\r\n");
@@ -206,10 +250,11 @@ void serve_dynamic(int fd, char *filename, char *cgiargs)
     sprintf(buf, "Server: Tiny Web Server\r\n");
     Rio_writen(fd, buf, strlen(buf));
 
-    if (Fork() == 0) { /* Child */
+    if (Fork() == 0)
+    { /* Child */
         /* Real server would set all CGI vars here */
         setenv("QUERY_STRING", cgiargs, 1);
-        Dup2(fd, STDOUT_FILENO); /* Redirect stdout to client */
+        Dup2(fd, STDOUT_FILENO);              /* Redirect stdout to client */
         Execve(filename, emptylist, environ); /* Run CGI program */
     }
     Wait(NULL); /* Parent waits for and reaps child */
